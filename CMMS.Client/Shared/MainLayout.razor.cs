1cbf59309fa51cd5b3af494fa69870f5f6ef4f74
TRAN KIET (TRẦN KIỆT) (Comp_HEV) <kiet.trantuan@aquavietnam.vn>
2026-05-05 11:44:46 +0700

    Init_v2

--- CMMS.Client/Shared/MainLayout.razor.cs
+++ CMMS.Client/Shared/MainLayout.razor.cs
@@ -1,9 +1,18 @@
 ﻿
+using Microsoft.AspNetCore.Components;
+
 namespace CMMS.Client.Shared
 {
+
     public partial class MainLayout
     {
-      
+        [Inject] NavigationManager NavigationManager { get; set; }
+        private string GetLink(string url)
+        {
+            var basePath = NavigationManager.BaseUri.TrimEnd('/');
+            return $"{basePath}{url}";
+        }
+
     }
 }
 
